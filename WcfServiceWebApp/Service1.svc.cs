d6b0db73c401ab17f6f2b800740dc64b700e6f81
khlupnov <m.khlupnov@gmail.com>
2020-06-01 17:19:54 +0300

    Initial

---
+++ WcfServiceWebApp/Service1.svc.cs
@@ -0,0 +1,10 @@
+﻿namespace WcfServiceWebApp
+{
+    public class Service1 : IService1
+    {
+        public string Hello(string name)
+        {
+            return string.Format($"Hello {name} from {System.Net.Dns.GetHostName()}!");
+        }
+    }
+}
